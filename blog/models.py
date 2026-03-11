16f254d76d4e04660ba9578667cac6607a7dfbcf
kjyyjk <rlawnsdud920@gmail.com>
2026-03-11 18:03:43 +0900

    feat: 카테고리, 게시글, 게시글 이미지, 고정 게시글 model 정의
    - 게시글 삭제 즉시 이미지 삭제하면 추후 삭제 게시글 복구 등의 상황에서 이미지 복구 관련 문제가 발생할 수 있다. 따라서 post_image 테이블의 post 외래키를 nullable, setnull로 설정.
    추후 고아 사진 및 삭제된 게시글의 이미지들을 별도로 관리할 수 있다.

---
+++ blog/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.db.models import CASCADE, SET_NULL
+
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=CASCADE)
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    content = models.TextField()
+    preview_image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=SET_NULL, null=True)
+    path = models.ImageField()
+    capacity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Pin(models.Model):
+    post = models.ForeignKey(Post, on_delete=CASCADE)
+    sort_order = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
