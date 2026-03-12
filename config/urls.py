b182bf429aceb0019b7eec4acf397938c5a73497
emfpdlzj <redryan@ajou.ac.kr>
2026-03-12 16:54:57 +0900

    chore: 게시물 작성 테스트용 html 추가

--- config/urls.py
+++ config/urls.py
@@ -5,11 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-<<<<<<< HEAD
     path("admin/blog/", include("blog.urls.admin_urls")), #admin보다 위에 와야함
-=======
-    path("blog/admin/", include("blog.urls.admin")),
->>>>>>> a849016 (blog/admin/ url 추가)
     path("admin/", admin.site.urls),
     path("posts/", include("blog.urls.public_urls")),
 ]
