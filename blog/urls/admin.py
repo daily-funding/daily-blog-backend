2fb631cbaaf8e193308f325f125179849dabb3ba
emfpdlzj <redryan@ajou.ac.kr>
2026-03-12 16:54:26 +0900

    posts/, posts/create/ url 추가

--- blog/urls/admin.py
+++ blog/urls/admin.py
@@ -8,6 +8,8 @@ from blog.views.admin_post_views import (
 app_name = "blog"
 
 urlpatterns = [
-    path("posts/", admin_post_list_view, name="admin-post-list"),
-    path("posts/create/", admin_post_create_view, name="admin-post-create"),
+    path("posts/", admin_post_list_view, name="admin-post-list"),  # 게시물 목록
+    path(
+        "posts/create/", admin_post_create_view, name="admin-post-create"
+    ),  # 게시물 작성
 ]
