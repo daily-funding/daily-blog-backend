1979ee77d1b529a130ac7fce3b6381f3843123ae
emfpdlzj <redryan@ajou.ac.kr>
2026-03-12 15:43:27 +0900

    feat:관리자 게시물 생성 서비스 구현

---
+++ blog/services/post_create_service.py
@@ -0,0 +1,13 @@
+from blog.models import Post
+
+
+def create_post(form, user):
+    """
+    관리자 게시물 생성 서비스
+    """
+
+    post = form.save(commit=False) # author 설정 전 저장 막음
+    post.author = user # author 설정
+    post.save()
+
+    return post
