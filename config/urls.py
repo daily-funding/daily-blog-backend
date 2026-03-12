08f7f2a13c3fee4f349231f2f7838313f6e10fb5
emfpdlzj <redryan@ajou.ac.kr>
2026-03-12 17:51:14 +0900

    refactor: url을 urls.py에서 관리하도록 변경

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("admin/blog/", include("blog.urls.admin_urls")), #admin보다 위에 와야함
+    path("admin/blog/", include("blog.urls.admin")),
     path("admin/", admin.site.urls),
-    path("posts/", include("blog.urls.public_urls")),
+    path("posts/", include("blog.urls.public")),
 ]
 
 
