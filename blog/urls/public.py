f709af44c5a463117fc3a7111698c156e181153d
emfpdlzj <redryan@ajou.ac.kr>
2026-03-12 18:12:07 +0900

    chore: 유저 url에서 필요 없는 부분 삭제

--- blog/urls/public.py
+++ blog/urls/public.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from blog.views.post_view import PostView
 
-app_name = "blog"
 
 urlpatterns = [
-    path("<int:post_id>/", PostView.as_view(), name="post-detail"),
+    path("<int:post_id>/", PostView.as_view()),
 ]
