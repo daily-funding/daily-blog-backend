1110583be220b0527872a4601d88d4f13b355fb2
kjyyjk <rlawnsdud920@gmail.com>
2026-03-13 14:10:31 +0900

    refactor: 미사용 import 제거

--- config/exceptions.py
+++ config/exceptions.py
@@ -1,4 +1,4 @@
-from rest_framework.status import HTTP_500_INTERNAL_SERVER_ERROR, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
 
@@ -14,6 +14,8 @@ def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
     if response is not None:
         return response
+
+    # django가 500 에러 처리
     return None
 
 
