ff853204ab73c4a228b35887d21e326143c5a153
emfpdlzj <redryan@ajou.ac.kr>
2026-03-13 15:49:25 +0900

    chore: 머지 오류 수정

--- blog/urls/public_urls.py
+++ blog/urls/public_urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-<<<<<<< HEAD
+
 from blog.views.post_views import PostDetailView, PostListView
 
 urlpatterns = [
     path("<int:post_id>/", PostDetailView.as_view()),
     path("", PostListView.as_view()),
-=======
-from blog.views.post_view import PostView
 
-
-urlpatterns = [
-    path("<int:post_id>/", PostView.as_view()),
->>>>>>> 2790c28 (chore: url 파일명 수정)
 ]
