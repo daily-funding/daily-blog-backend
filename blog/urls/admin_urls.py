74878b0cef8c9057e601a729b30f033f47c7a5bc
emfpdlzj <redryan@ajou.ac.kr>
2026-03-13 14:31:22 +0900

    feat: 게시물 자세히보기 Url 설정

--- blog/urls/admin_urls.py
+++ blog/urls/admin_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from blog.views.admin_post_views import (
     admin_post_create_view,
     admin_post_list_view,
+    admin_post_detail_view,
 )
 from blog.views.admin_image_upload_views import admin_image_upload_view
 
@@ -13,6 +14,11 @@ urlpatterns = [
         "posts/create/",
         admin_post_create_view,
         name="admin-post-create",
+    ),
+    path(
+        "posts/<int:post_id>/",
+        admin_post_detail_view,
+        name="admin-post-detail",
     ),
     path("images/upload/", admin_image_upload_view, name="admin-image-upload"),
 ]
