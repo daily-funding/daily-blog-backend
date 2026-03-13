cf9839ff1f990635d5946829403d5e0660d23269
emfpdlzj <redryan@ajou.ac.kr>
2026-03-13 13:25:49 +0900

    chore: 중복되는 staff_member_required 삭제

--- blog/urls/admin_urls.py
+++ blog/urls/admin_urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.admin.views.decorators import staff_member_required
 from blog.views.admin_post_views import (
     admin_post_create_view,
     admin_post_list_view,
@@ -8,10 +7,10 @@ from blog.views.admin_post_views import (
 app_name = "blog"
 
 urlpatterns = [
-    path("posts/", staff_member_required(admin_post_list_view), name="admin-post-list"),
+    path("posts/", admin_post_list_view, name="admin-post-list"),
     path(
         "posts/create/",
-        staff_member_required(admin_post_create_view),
+        admin_post_create_view,
         name="admin-post-create",
     ),
 ]
