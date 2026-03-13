88dcb6ce66adec40dd37e1504ed84e321de451bf
kjyyjk <rlawnsdud920@gmail.com>
2026-03-13 10:03:36 +0900

    feat: 게시글 목록 조회 api 라우팅 구현

--- blog/urls/public.py
+++ blog/urls/public.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from blog.views.post_view import PostView
-
+from blog.views.post_views import PostDetailView, PostListView
 
 urlpatterns = [
-    path("<int:post_id>/", PostView.as_view()),
+    path("<int:post_id>/", PostDetailView.as_view()),
+    path("", PostListView.as_view()),
 ]
