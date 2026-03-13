c19e575d89a07edbe06e44ebea31c9ac7c686dfa
kjyyjk <rlawnsdud920@gmail.com>
2026-03-13 10:54:35 +0900

    refactor: 게시물 커스텀 페이지네이션 클래스 사용

--- blog/views/post_views.py
+++ blog/views/post_views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from blog.models import Post
+from blog.paginations.PostPagination import PostPagination
 from blog.serializers.post_serializers import PostDetailSerializer, PostListSerializer
 
 class PostDetailView(APIView):
@@ -23,7 +23,7 @@ class PostListView(APIView):
             if key in ["category_id"]
         }
         posts = Post.objects.filter(**filters).order_by("-created_at")
-        pagination = PageNumberPagination()
+        pagination = PostPagination()
         paginated_posts = pagination.paginate_queryset(posts, request)
         serializer = PostListSerializer(paginated_posts, many=True)
         return pagination.get_paginated_response(serializer.data)
