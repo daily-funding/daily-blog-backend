003b636ab30177abe27d559ffe1ce79d185dca92
kjyyjk <rlawnsdud920@gmail.com>
2026-03-13 13:29:21 +0900

    refactor: PostPagination 클래스 이동

--- blog/views/post_views.py
+++ blog/views/post_views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import get_object_or_404
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from blog.models import Post
-from blog.paginations.PostPagination import PostPagination
 from blog.serializers.post_serializers import PostDetailSerializer, PostListSerializer
 from blog.util.request_parser import get_query_string_filter
+
+
+class PostPagination(PageNumberPagination):
+    page_size = 6
 
 
 class PostDetailView(APIView):
@@ -21,7 +25,11 @@ class PostListView(APIView):
     def get(self, request):
         filters = get_query_string_filter(request, "category_id")
         try:
-            posts = Post.objects.select_related('category').filter(**filters).order_by("-created_at")
+            posts = (
+                Post.objects.select_related("category")
+                .filter(**filters)
+                .order_by("-created_at")
+            )
         except ValueError as e:
             return Response({"error": str(e)}, status=400)
 
