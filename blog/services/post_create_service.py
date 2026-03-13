91c43f6ac80c7cdbd7e34548508976ca973c110f
emfpdlzj <redryan@ajou.ac.kr>
2026-03-13 15:46:35 +0900

    chore: 주석 수정

--- blog/services/post_create_service.py
+++ blog/services/post_create_service.py
@@ -1,19 +1,8 @@
 from blog.models import Post
 
-<<<<<<< HEAD
 # 관리자 게시물 생성 서비스
 def create_post(*, validated_data, author):
     return Post.objects.create(
         author=author,
         **validated_data, # 추후 데이터타입 확정되면 변경
-=======
-
-def create_post(*, validated_data, author):
-    """
-    관리자 게시물 생성 서비스
-    """
-    return Post.objects.create(
-        author=author,
-        **validated_data,
->>>>>>> ae81f26 (refactor: service가 ModelForm 구현에 의존하지 않도록 수정)
     )
