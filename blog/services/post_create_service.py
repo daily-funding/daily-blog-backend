6dc2cb95ee9755410c8bf26e4682824c67bd7d20
emfpdlzj <redryan@ajou.ac.kr>
2026-03-14 18:24:24 +0900

    fix: 길어지는 로직 함수로 분리

--- blog/services/post_create_service.py
+++ blog/services/post_create_service.py
@@ -2,6 +2,8 @@ import logging
 import re
 
 from django.conf import settings
+from django.db import transaction
+
 from blog.models import Post, PostImage
 
 logger = logging.getLogger(__name__)
@@ -15,13 +17,15 @@ IMG_SRC_PATTERN = re.compile(
 
 # 이미지 경로 문자열 리스트 반환
 def extract_post_image_paths_from_html(content: str) -> list[str]:
+    if not content:
+        return []
+
     image_paths = []
+    root = settings.POST_IMAGE_UPLOAD_ROOT
+    root_without_slash = root.rstrip("/")
 
-    #  src 값만 추출함
+    # src 값만 추출함
     for src in IMG_SRC_PATTERN.findall(content):
-        root = settings.POST_IMAGE_UPLOAD_ROOT
-        root_without_slash = root.rstrip("/")
-
         if root in src:
             image_paths.append(src[src.index(root) :])
         elif root_without_slash in src:
@@ -31,34 +35,42 @@ def extract_post_image_paths_from_html(content: str) -> list[str]:
     return image_paths
 
 
+# 본문 내 이미지와 PostImage를 연결
+def bind_post_images_to_post(post: Post) -> None:
+    image_paths = extract_post_image_paths_from_html(post.content)
+
+    if not image_paths:
+        return
+
+    unique_image_paths = list(dict.fromkeys(image_paths))
+
+    updated_count = PostImage.objects.filter(
+        post__isnull=True,
+        path__in=unique_image_paths,
+    ).update(post=post)
+
+    if updated_count != len(unique_image_paths):
+        logger.warning(
+            "일부 추출된 이미지가 매핑에 실패했습니다.",
+            extra={
+                "post_id": post.id,
+                "extracted_count": len(image_paths),
+                "unique_extracted_count": len(unique_image_paths),
+                "updated_count": updated_count,
+                "image_paths": image_paths,
+                "unique_image_paths": unique_image_paths,
+            },
+        )
+
+
 # 관리자 게시물 생성 서비스
+@transaction.atomic
 def create_post(*, validated_data, author):
     post = Post.objects.create(
         author=author,
         **validated_data,
     )
 
-    image_paths = extract_post_image_paths_from_html(post.content)
-
-    if image_paths:
-        unique_image_paths = list(dict.fromkeys(image_paths))
-
-        updated_count = PostImage.objects.filter(
-            post__isnull=True,
-            path__in=unique_image_paths,
-        ).update(post=post)
-
-        if updated_count != len(unique_image_paths):
-            logger.warning(
-                "PostImage mapping failed",
-                extra={
-                    "post_id": post.id,
-                    "extracted_count": len(image_paths),
-                    "unique_extracted_count": len(unique_image_paths),
-                    "updated_count": updated_count,
-                    "image_paths": image_paths,
-                    "unique_image_paths": unique_image_paths,
-                },
-            )
+    bind_post_images_to_post(post)
 
     return post
