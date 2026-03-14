3a08d3610999281456bbb195d0a02bf30cfb297b
kjyyjk <rlawnsdud920@gmail.com>
2026-03-14 19:07:32 +0900

    feat: 고정 게시물 api 라우터 연결

--- blog/urls/public_urls.py
+++ blog/urls/public_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog.views.post_views import PostDetailView, PostListView
+from blog.views.post_views import PostDetailView, PostListView, TopPostListView
 
 urlpatterns = [
     path("<int:post_id>/", PostDetailView.as_view()),
+    path("top/", TopPostListView.as_view()),
     path("", PostListView.as_view()),
 ]
