9e64b580c7d06219f697d9467659e03146b9a875
kjyyjk <rlawnsdud920@gmail.com>
2026-03-16 09:02:51 +0900

    feat: category 목록 조회 라우터 연결

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("admin/blog/", include("blog.urls.admin_urls")),  # admin보다 위에 와야함
     path("admin/", admin.site.urls),
     path("posts/", include("blog.urls.post_urls")),
+    path("categories/", include("blog.urls.category_urls")),
     path("ckeditor/", include("ckeditor_uploader.urls")),  # ckeditor 경로
 ]
 
