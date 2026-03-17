8ceaf6f00fe1c746f7bb4036412c40588e59bbc9
emfpdlzj <redryan@ajou.ac.kr>
2026-03-17 17:21:25 +0900

    feat: 게시물 삭제 로직

---
+++ blog/services/post_delete_service.py
@@ -0,0 +1,52 @@
+import logging
+from datetime import timedelta
+
+from django.db import transaction
+from django.utils import timezone
+
+from blog.models import Post, PostImage
+
+logger = logging.getLogger(__name__)
+
+# 게시물 삭제 후 이미지 post_id, updated_at 수정
+def mark_post_images_for_cleanup(post: Post) -> None:
+    post_images = PostImage.objects.filter(post=post)
+
+    for post_image in post_images:
+        post_image.post = None
+        post_image.save(update_fields=["post", "updated_at"])
+
+
+def delete_post_preview_image(post: Post) -> None:
+    if not post.preview_image or not post.preview_image.name:
+        return
+
+    file_name = post.preview_image.name
+    storage = post.preview_image.storage
+
+    try:
+        storage.delete(file_name)
+    except Exception:
+        logger.exception(
+            "Post preview image delete failed",
+            extra={
+                "post_id": post.id,
+                "file_name": file_name,
+            },
+        )
+
+
+@transaction.atomic
+def delete_post(post: Post) -> None:
+    mark_post_images_for_cleanup(post)
+    delete_post_preview_image(post)
+    post.delete()
+
+
+def get_cleanup_target_post_images():
+    cutoff = timezone.now() - timedelta(hours=24)
+
+    return PostImage.objects.filter(
+        post__isnull=True,
+        updated_at__lte=cutoff,
+    )
