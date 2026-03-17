9d5eeed5a0e7a427027db45cb2cd493d1207745c
emfpdlzj <redryan@ajou.ac.kr>
2026-03-17 16:33:29 +0900

    fix: 문자열 표시 오류 해결

--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,9 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=SET_NULL, null=True)
