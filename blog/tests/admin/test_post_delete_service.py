a58b02b39414fada3e86f17ad28f8e6ca8247f92
emfpdlzj <redryan@ajou.ac.kr>
2026-03-19 06:39:13 +0900

    test: 게시물삭제 테스트 추가

--- blog/tests/admin/test_post_delete_service.py
+++ blog/tests/admin/test_post_delete_service.py
@@ -1,11 +1,12 @@
 # 게시물 삭제 시 PostImage.post = null / updated_at 갱신 테스트
 from datetime import timedelta
+from unittest.mock import Mock
 
 import pytest
 from django.utils import timezone
 
 from blog.models import PostImage
-from blog.services.post_delete_service import delete_post
+from blog.services.post_delete_service import delete_post, delete_post_preview_image
 
 
 @pytest.mark.django_db
@@ -26,3 +27,18 @@ def test_delete_post_marks_post_images_for_cleanup(post, post_image_factory):
 
     assert post_image.post is None
     assert post_image.updated_at > old_updated_at
+
+
+@pytest.mark.django_db
+def test_delete_post_preview_image_uses_storage_delete_without_path_access(
+    post, preview_image_file
+):
+    post.preview_image = preview_image_file
+    post.preview_image.name = "images/posts/preview.png"
+    post.preview_image.storage.delete = Mock()
+
+    delete_post_preview_image(post)
+
+    post.preview_image.storage.delete.assert_called_once_with(
+        "images/posts/preview.png"
+    )
