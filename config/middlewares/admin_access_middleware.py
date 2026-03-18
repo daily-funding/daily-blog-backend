f2d3b521bfc79b01db3e572bc1f222037c06beb2
emfpdlzj <redryan@ajou.ac.kr>
2026-03-18 17:46:42 +0900

    feat: 어드민 접근시 404 뜨도록 설정

---
+++ config/middlewares/admin_access_middleware.py
@@ -0,0 +1,27 @@
+from django.http import Http404
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+class AdminAccessMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.admin_login_path = reverse("admin:login")
+
+    def __call__(self, request):
+        path = request.path
+
+        if not path.startswith("/admin/"):
+            return self.get_response(request)
+
+        if not request.user.is_authenticated:
+            if path != self.admin_login_path:
+                return redirect(
+                    f"{self.admin_login_path}?next={request.get_full_path()}"
+                )
+            return self.get_response(request)
+
+        if not request.user.is_staff:
+            raise Http404("페이지를 찾을 수 없습니다.")
+
+        return self.get_response(request)
